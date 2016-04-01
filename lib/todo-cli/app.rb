770cb349f1065e927c159ae47b8762b94ca3836d
Grant Ammons <gammons@gmail.com>
2016-04-01 15:36:04 -0400

    Extract formatting to a formatter

--- lib/todo-cli/app.rb
+++ lib/todo-cli/app.rb
@@ -1,6 +1,5 @@
 module Todo
   class App
-    PADDING = 3
 
     def initialize
       @store = Store.new
@@ -48,56 +47,7 @@ module Todo
 
     def list_todos(args)
       filtered = Filter.new(@store.todos, args).filter
-
-      longest_projects = find_longest_projects + PADDING
-      longest_contexts = find_longest_contexts + PADDING
-      longest_subject = find_longest_subject + PADDING
-
-      filtered.each_with_index do |todo, i|
-        printf "%-3s %-5s %-#{longest_projects}s %-#{longest_contexts}s %-10s %s\n",
-          todo.id,
-          format_completed(todo),
-          format_projects(todo),
-          format_contexts(todo),
-          format_due(todo),
-          format_subject(todo)
-      end
-    end
-
-    private
-
-    def find_longest_projects
-      @store.todos.map {|t| format_projects(t).length }.max
-    end
-
-    def find_longest_contexts
-      @store.todos.map {|t| format_contexts(t).length }.max
-    end
-
-    def find_longest_subject
-      @store.todos.map {|t| format_subject(t).length }.max
-    end
-
-    def format_completed(todo)
-      todo.completed ? "[x]" : "[ ]"
-    end
-
-    def format_projects(todo)
-      return "" if todo.projects.empty?
-      "[" + todo.projects.join(", ") + "]"
-    end
-
-    def format_contexts(todo)
-      return "" if todo.contexts.empty?
-      "(" + todo.contexts.join(", ") + ")"
-    end
-
-    def format_due(todo)
-      todo.due.to_s.colorize(:blue)
-    end
-
-    def format_subject(todo)
-      todo.subject.colorize(:yellow)
+      Formatter.new(filtered).print!
     end
   end
 end
---
+++ lib/todo-cli/formatter.rb
@@ -0,0 +1,73 @@
+module Todo
+  class Formatter
+    PADDING = 3
+
+    def initialize(todos)
+      @todos = todos
+    end
+
+    def print!
+      longest_projects = find_longest_projects + PADDING
+      longest_contexts = find_longest_contexts + PADDING
+      longest_subject = find_longest_subject + PADDING
+      longest_due = find_longest_due + PADDING
+
+      @todos.each_with_index do |todo, i|
+        printf "%-3s %-5s %-#{longest_projects}s %-#{longest_contexts}s %-#{longest_due}s %s\n",
+          todo.id,
+          format_completed(todo),
+          format_projects(todo),
+          format_contexts(todo),
+          format_due(todo),
+          format_subject(todo)
+      end
+    end
+
+    private
+
+    def find_longest_projects
+      @todos.map {|t| format_projects(t).length }.max
+    end
+
+    def find_longest_contexts
+      @todos.map {|t| format_contexts(t).length }.max
+    end
+
+    def find_longest_subject
+      @todos.map {|t| format_subject(t).length }.max
+    end
+
+    def find_longest_due
+      @todos.map {|t| format_due(t).length }.max
+    end
+
+    def format_completed(todo)
+      todo.completed ? "[x]" : "[ ]"
+    end
+
+    def format_projects(todo)
+      return "" if todo.projects.empty?
+      "[" + todo.projects.join(", ") + "]"
+    end
+
+    def format_contexts(todo)
+      return "" if todo.contexts.empty?
+      "(" + todo.contexts.join(", ") + ")"
+    end
+
+    def format_due(todo)
+      case todo.due
+      when Date.today.to_s
+        "today".colorize(:blue)
+      when (Date.today + 1).to_s
+        "tomorrow".colorize(:blue)
+      else
+        todo.due.to_s.colorize(:blue)
+      end
+    end
+
+    def format_subject(todo)
+      todo.subject.colorize(:yellow)
+    end
+  end
+end
